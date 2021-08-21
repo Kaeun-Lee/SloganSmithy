c2bc7839a0b3a5723f3e9a0f778f80dd069f5c03
KaEun Lee <54276004+devkani@users.noreply.github.com>
2021-08-21 16:13:44 +0900

    Delete accountapp directory

--- accountapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class main_slogan(models.Model):
-    content = models.CharField(max_length=255, null=False)
-    date = models.DateField(auto_now=True) 
